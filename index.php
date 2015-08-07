c40cbc3a0ae679ceca639531693e4f8d3bbf1d44
akshaypatilofficial <akshaypatilofficial93@gmail.com>
2015-08-07 20:41:01 +0530

    Insert and update functionality 10/23

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'core/init.php';
 
+/*
 $user = DB::getInstance()->get('users', array('username', '=', 'akshay'));
 
 if(!$user->count())
@@ -12,4 +13,16 @@ else
 	echo $user->first()->username;
 }
 
+*/
+/*
+$user = DB::getInstance()->insert('users', array(
+	'username' => 'Dale',
+	'password' => 'password',
+	'salt' => 'salt'
+));
+*/
+$user = DB::getInstance()->update('users', 5, array(
+	'password' => 'newpassword',
+	'name' => 'DaleBitch'
+));
 ?>
